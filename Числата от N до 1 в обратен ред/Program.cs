d38d0cb7665c5328ea8c93ac3f481524199e6868
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-01-10 10:29:57 +0200

    2 problem

---
+++ Числата от N до 1 в обратен ред/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Числата_от_N_до_1_в_обратен_ред
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = number; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
