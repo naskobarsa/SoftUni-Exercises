ed0877bc31900a2e5e6ae7c93f5f1bd245a13195
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-01-10 10:27:24 +0200

    problem 2 resolved

--- 1 до N през 3/Program.cs
+++ 1 до N през 3/Program.cs
@@ -7,7 +7,7 @@ namespace _1_до_N_през_3
         static void Main(string[] args)
         {
             int number = int.Parse(Console.ReadLine());
-            for (int i = 1; i < number; i+=3)
+            for (int i = 1; i <= number; i+=3)
             {
                 Console.WriteLine(i);
             }
