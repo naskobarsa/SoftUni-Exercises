762f6e6134c7c0be8ee75b8779fb945d28918309
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-01-10 09:53:39 +0200

    problem 1 resolved

---
+++ 1 до N през 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_до_N_през_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i < number; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+    }
+}
