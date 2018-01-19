e046afe73462e20bea88b7f6f6b2f1ac4ae3845a
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-01-19 11:02:33 +0200

    Add files via upload

--- Programming Fundamentals/problem2/Program.cs
+++ Programming Fundamentals/problem2/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Programming_Fundamentals
+namespace problem2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
         }
     }
 }
